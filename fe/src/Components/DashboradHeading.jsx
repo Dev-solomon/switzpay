df011c400f4790fabb5357dd687c81be320731aa
Solomon Ntia <107444454+Dev-solomon@users.noreply.github.com>
2023-03-30 11:13:44 +0100

    Delete fe directory

--- fe/src/Components/DashboradHeading.jsx
+++
@@ -1,9 +0,0 @@
-function DashboradHeading(props) {
-  return (
-    <div className=" text-base md:text-[22px] lg:text-[26px]">
-      <h2>{props.heading} </h2>
-    </div>
-  );
-}
-
-export default DashboradHeading
